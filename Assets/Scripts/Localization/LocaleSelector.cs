641c1b635be79afd5a5af3e57e096122a17b3d33
marccorteslon <marc.cortes@enti.cat>
2026-04-29 12:09:59 +0200

    Localization Implemented

---
+++ Assets/Scripts/Localization/LocaleSelector.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using System.Collections;
+
+public class LocaleSelector : MonoBehaviour
+{
+    private void Start()
+    {
+        int ID = PlayerPrefs.GetInt("LocaleKey", 0);
+        ChangeLocale(ID);
+    }
+
+    private bool active = false;
+
+    public void ChangeLocale(int localeID)
+    {
+        if (active == true)
+            return;
+
+        StartCoroutine(SetLocale(localeID));
+    }
+
+    IEnumerator SetLocale(int _localeID)
+    {
+        active = true;
+
+        yield return LocalizationSettings.InitializationOperation;
+
+        LocalizationSettings.SelectedLocale =
+            LocalizationSettings.AvailableLocales.Locales[_localeID];
+
+        PlayerPrefs.SetInt("LocaleKey", _localeID);
+
+        active = false;
+    }
+}
