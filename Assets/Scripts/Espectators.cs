adeac0276793503bf946ce35a0ee0d5b1764194f
marccorteslon <marc.cortes@enti.cat>
2026-05-27 00:07:41 +0200

    Map improves

--- Assets/Scripts/Espectators.cs
+++ Assets/Scripts/Espectators.cs
@@ -3,29 +3,29 @@ using UnityEngine;
 public class Espectators : MonoBehaviour
 {
     [Header("Movimiento")]
-    public float altura = 2f;          // Distancia máxima arriba/abajo
-    public float velocidad = 2f;       // Velocidad del movimiento
+    public float altura = 2f;
+    public float velocidad = 2f;
 
     [Header("Fase")]
-    public bool faseInvertida = false; // Si está activado, empieza al revés
+    public bool faseInvertida;
 
     private Vector3 posicionInicial;
 
     void Start()
     {
         posicionInicial = transform.position;
+
+        // 50% de probabilidad de true o false
+        faseInvertida = Random.value > 0.5f;
     }
 
     void Update()
     {
-        // Calcula el movimiento usando seno
         float offset = Mathf.Sin(Time.time * velocidad);
 
-        // Invierte la fase para que unos suban mientras otros bajan
         if (faseInvertida)
             offset *= -1f;
 
-        // Aplica movimiento vertical
         transform.position = posicionInicial + Vector3.up * offset * altura;
     }
 }
